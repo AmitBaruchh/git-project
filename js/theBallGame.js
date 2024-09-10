61191e1018e03e9036f831b2f535d28cb1b09368
Amit Baruch <amitbaruch08@gmail.com>
2024-09-10 20:12:01 +0300

    Update onBallClick function to limit ball size

--- js/theBallGame.js
+++ js/theBallGame.js
@@ -2,6 +2,7 @@
 
 function onBallClick(elBall) {
     var newBallSize = +elBall.innerText + 50
+    newBallSize = newBallSize > 400 ? 100 : newBallSize
     elBall.style.width = newBallSize + 'px'
     elBall.style.height = newBallSize + 'px'
     elBall.innerText = newBallSize
